15e8eff1d48b3cc90e760f550161060598b08251
ron-rivest <rivest@mit.edu>
2016-01-10 13:31:45 -0800

    added code

---
+++ dropper.py
@@ -0,0 +1,103 @@
+# program to drop lowest grade for a student (dropper.py)
+# Ron Rivest
+# 1/9/16
+
+# This program is called by student ranking program rank.py
+
+# You can change the following default policy, or have rank.py
+# pass along a different policy, in order to change the behavior
+# of this routine.
+# Set this variable to the empty list to have no items dropped.
+DEFAULT_DROP_POLICY = [ (2, "H1", "H2", "H3", "H4", "H5", "H6"),      # drop lowest two psets
+                        (1, "Q1", "Q2"),                               # drop lowest one quiz
+                        (2, "pset1", "pset2", "pset3", "pset4", "pset5", "pset6"), # variant spellings?
+                        (1, "quiz1", "quiz2")
+                      ]
+
+MISSING = "--"
+
+def isnonnumeric(x):
+    """ Return True if x looks to be non-numeric """
+    try:
+        x = float(x)
+    except ValueError:
+        return True
+    return False
+
+def drop(names, weights, scores, drop_policy=DEFAULT_DROP_POLICY):
+    """
+    Drop some of the student scores and return resulting modified scores.
+    Dropping is effected by replacing some values by MISSING.
+    Drop policy has the form (by example):
+        [ (2, "H1", "H2", "H3", "H4", "H5"),           # drop lowest two homeworks
+          (1, "Q1", "Q2")                              # drop lowest quiz
+        ]                        
+    Note that here "lowest" is intended to refer to scores, not grades.
+    """
+    print_drop_policy(names, drop_policy)
+    new_scores = []
+    for score_row in scores:
+        for drop_policy_item in drop_policy:
+            score_row = process_drop_policy_item(names, weights, score_row, drop_policy_item)
+        new_scores.append(score_row)
+    return new_scores
+
+def compare(x, y):
+    """
+    Return a negative, zero, or positive value if x < y, x==y, or x>y.
+    Treat MISSING or nonnumeric is small.
+    Here x, y are pairs (x0, x1), (y0, y1) e.g. (0.33, "H1") or ("--", "Q2").
+    """
+    x0, x1 = x
+    if x0 == MISSING or isnonnumeric(x0):
+        x0 = float("-inf")
+    y0, y1 = y
+    if y0 == MISSING or isnonnumeric(y0):
+        y0 = float("-inf")
+    if x0 < y0:
+        return -1
+    if x0 > y1:
+        return 1
+    if x1 < y1:
+        return -1
+    if x1 > y1:
+        return 1
+    return 0
+
+def process_drop_policy_item(names, weights, score_row, drop_policy_item):
+    """
+    Return score_row after effecting given drop_policy item.
+    Here drop_policy_item is, e.g. (1, "Q1", "Q2") (E.g. drop lowest quiz.)
+    """
+    k = drop_policy_item[0]
+    drop_policy_names = drop_policy_item[1:]
+    common_names = [name for name in drop_policy_names if name in names]
+    d = dict()
+    for name, score in zip(names, score_row):
+        d[name] = score
+    L = [(d.get(name,MISSING), name) for name in common_names]
+    L = sorted(L, cmp=compare) 
+    L = L[:k]            # list of items to drop
+    for score, name in L:
+        d[name] = MISSING             # i.e. dropped
+    score_row = [ d[name] for name in names ]
+    return score_row
+
+def print_drop_policy(names, drop_policy=DEFAULT_DROP_POLICY):
+    """
+    Print out the specified drop policy for dropping items from a student's record 
+    for grading purposes.
+    """
+    print "Effecting the following drop policy:"
+    for drop_policy_item in drop_policy:
+        k = drop_policy_item[0]
+        drop_policy_names = drop_policy_item[1:]
+        common_names = [name for name in drop_policy_names if name in names]
+        print "    Dropping lowest %d from:"%k,
+        for name in common_names:
+            print name,
+        print        
+    
+
+
+        
---
+++ rank.py
@@ -0,0 +1,330 @@
+# student ranking program (rank.py)
+# Ron Rivest
+# 1/9/16
+
+"""
+This is a program to produce an overall student ranking, given
+as input a spreadsheet given student scores on various grade
+components, and given weights for those components.
+"""
+
+""" (Distributed under MIT License) """
+
+import argparse
+import copy
+import csv
+import sys
+
+import dropper
+
+##############################################################################
+## Beginning of ranking program
+##############################################################################
+
+def read_grades(input_filename, skiprows=0, maxrows=10000):
+    """ 
+    Read grades file at given input_filename.
+    Return:
+       list of column names, 
+       list of column weights
+           non-numeric --> not part of grade
+           numeric     --> weight for weighting in grade
+       rows of student grades.
+
+    The input file format is:
+        'skiprows' rows to be skipped
+        a header row giving column names
+        a weight row given component weights for components to be included
+          (a zero, non-numeric or missing weight means to ignore this column for grade)
+        a number of rows of data, one per student
+    At most the first 'maxrows' of student grade data will be read.
+
+    BE CAREFUL: the values read from csv are all STRINGS, and must be 
+    converted to numeric data types as appropriate.
+    """
+    print "Reading input file:", input_filename
+    # rU is universal newline mode
+    with open(input_filename, 'rU') as csvfile:   
+        gradereader = csv.reader(csvfile)
+        skipped_rows = []
+        for _ in range(skiprows):
+            skipped_rows.append(gradereader.next())
+        names = gradereader.next()        
+        names = [ name.strip() for name in names ]
+        weights = gradereader.next()
+        weights = [ convert_to_float_if_possible(w,0) for w in weights ]
+        grades = []
+        for row in gradereader:
+            grades.append(row)
+        grades = grades[:maxrows]
+        return names, weights, grades
+
+# MISSING DATA (marked by sentinel value "--")
+MISSING = "--"
+def ismissing(x):
+    return (x == MISSING)
+
+def isnonnumeric(x):
+    """ Return True if x looks to be non-numeric """
+    try:
+        x = float(x)
+    except ValueError:
+        return True
+    return False
+
+def convert_to_float_if_possible(x, elsevalue=MISSING):
+    """ 
+    Return float version of value x, else elsevalue (MISSING or other specified value
+    if conversion fails 
+    """
+    if isnonnumeric(x):
+        return elsevalue
+    else:
+        return float(x)
+
+def convert_data(weights, grades):
+    """
+    convert all grade data to floats, 
+    with MISSING for data that doesn't convert to a float.
+    """
+    n_rows = len(grades)
+    n_cols = len(weights)
+    for j in range(n_cols):
+        w = convert_to_float_if_possible(weights[j], 0)
+        if w>0:
+            for i in range(n_rows):
+                grades[i][j] = convert_to_float_if_possible(grades[i][j])
+
+def print_grade_components(names, weights):
+    print "Column names (with weights for those being included in grade):"
+    n_cols = len(weights)
+    for j in range(n_cols):
+        print "  %10s"%names[j],
+        w = convert_to_float_if_possible(weights[j],0)
+        if w>0:
+            print "weight", "%g"%w
+        else:
+            print "------"
+
+def datum_str(datum, width, sep):
+    """
+    Return string rep of data item.
+    """
+    if isnonnumeric(datum):
+        return ("%"+str(width)+"s"+sep)%datum
+    else:
+        if datum == int(float(datum)):
+            return ("%"+str(width)+"d"+sep)%int(float(datum))
+        else:
+            return ("%"+str(width)+".3f"+sep)%float(datum)
+
+def print_output(output, names, weights, data, best_stu_order,sep):
+    """
+    Print output to file (or equivalent I/O) "output".
+    Print names, weights, and then the rows in data (one per student).
+    The order of the rows is given by best_stu_order.
+    Sep is what to put between data elements:
+        either "," (for csv use) or " " (for screen).
+    """
+    n_cols = len(names)
+
+    # first compute column widths for printing
+    width = [0]*(n_cols+1)
+    for col, name in enumerate(names):
+        width[col] = max(width[col],len(str(name.strip())))
+    for rank, stu in enumerate(best_stu_order):
+        for col in range(n_cols):
+            datum = data[stu][col]
+            width[col] = max(width[col],len(datum_str(datum, 0, sep)))
+
+    # now print 
+    for col, name, in enumerate(names):
+        print >>output, ("%"+str(width[col])+"s"+sep)%(name.strip()),
+    print >>output
+    # Data rows, one per student:
+    for rank, stu in enumerate(best_stu_order):
+        for col in range(n_cols):
+            datum = data[stu][col]
+            print >>output, datum_str(datum, width[col], sep),
+        print >>output
+
+def compute_scores(names, weights, grades):
+    """ 
+    Return scores: array of student x column with student scores (derived from rank within column)
+    """
+
+    n_stu = len(grades)           
+    n_cols = len(names)
+
+    students = range(n_stu)
+    
+    stu_per_comp = [ 0  for col in range(n_cols) ]
+    weight_per_stu = [ 0 for stu in students ]
+    beats = [ [ 0 for col in range(n_cols) ] for stu in students ]
+
+    # compare all pairs stu,stu2 of students
+    # beats[stu][col] is number of other students in same column this student beats
+    # where a student beats himself (+1) and beats others with same score by one-half (0.5)
+    for stu in students:
+        for col in range(n_cols):
+            w = convert_to_float_if_possible(weights[col],0)
+            if w>0:
+                d1 = grades[stu][col]
+                if not ismissing(d1):
+                    stu_per_comp[col] += 1
+                    weight_per_stu[stu] += weights[col]
+                    beats[stu][col] = 0
+                    for stu2 in students:
+                        d2 = grades[stu2][col]
+                        if not ismissing(d2):
+                            if stu == stu2:
+                                beats[stu][col] += 1.0
+                            elif d2 == d1:
+                                beats[stu][col] += 0.5
+                            elif d1 > d2:
+                                beats[stu][col] += 1.0
+
+    # normalize scores to [0,1] by dividing by number of students per component, plus one
+    # preserve missing or other data "as is"
+    scores =  [ [ 0 for col in range(n_cols) ] for stu in students ]
+    for stu in students:
+        for col in range(n_cols):
+            w = convert_to_float_if_possible(weights[col],0)
+            if w>0:
+                if not ismissing(grades[stu][col]):
+                    scores[stu][col] = beats[stu][col] / (float(stu_per_comp[col]) + 1.0)
+                else:
+                    scores[stu][col] = MISSING
+            else:
+                scores[stu][col] = grades[stu][col]
+    return scores
+
+def compute_ranks(names, weights, scores):
+    """
+    Compute weighted average score and new ranks.
+    """
+    n_cols = len(scores[0])
+    n_stu = len(scores)
+    students = range(n_stu)
+
+    # compute avg_score per student as weighted sum of component scores
+    wtd_score = [0 for stu in students]
+    for stu in students:
+        total = 0.0
+        total_weight = 0.0
+        for col in range(n_cols):
+            w = convert_to_float_if_possible(weights[col],0)
+            if w>0:
+                d = scores[stu][col]
+                if not ismissing(d):
+                    total += w * d
+                    total_weight += w
+        wtd_score[stu] = total / total_weight
+
+    L = sorted([ (wtd_score[stu], stu) for stu in students ], reverse=True)
+    stu_order = [ s for (an, s) in L ]
+    wtd_score = [ an for (an, s) in L ]
+                
+    ranks = range(n_stu)
+    return stu_order, wtd_score, ranks, scores
+
+def augment(names, weights, grades, scores, stu_order, wtd_score):
+    """
+    Augment grades and scores to have two new columns
+    """
+    names = copy.deepcopy(names)
+    weights = copy.deepcopy(weights)
+    grades = copy.deepcopy(grades)
+    scores = copy.deepcopy(scores)
+    names.append("wtd_score")
+    weights.append(0)
+    names.append("rank")
+    weights.append(0)
+    for r, stu in enumerate(stu_order):
+        grades[stu].append(wtd_score[r])
+        grades[stu].append(r)
+        scores[stu].append(wtd_score[r])
+        scores[stu].append(r)
+    return names, weights, grades, scores
+
+def main():
+    print "---------------------------------------------"
+    print "-- Student ranking program (rank.py)       --"
+    print "-- Version 0.2 (1/9/16) Ronald L. Rivest   --"
+    print "---------------------------------------------"
+
+    # PARSE ARGUMENTS 
+    parser = argparse.ArgumentParser(description='Rank-order students based on performance.')
+    parser.add_argument('input_filename',help='csv file with header row, weight row, and then one row per student')
+    parser.add_argument('--skiprows',default=0,help='number of rows to skip before header row')
+    args = parser.parse_args()
+
+    input_filename = args.input_filename
+    skiprows = int(args.skiprows)
+    maxrows = 10000
+
+    # READ AND CLEAN UP DATA
+    names, weights, grades = read_grades(input_filename, skiprows, maxrows)
+    n_stu = len(grades)
+    n_cols = len(weights)
+    convert_data(weights, grades)
+    print_grade_components(names, weights)
+    print n_stu, "students"
+
+    # COMPUTE SCALED SCORES
+    # scores has one row per student, one column per original grades column
+    scores = compute_scores(names, weights, grades)
+
+    # COMPUTE WEIGHTED AVERAGE SCORES AND RANKS
+    stu_order, wtd_score, ranks, scores = compute_ranks(names, weights, scores)
+
+    names2, weights2, grades2, scores2 = augment(names, weights, grades, scores, stu_order, wtd_score)
+
+    # OUTPUT RESULTS
+    print "-"*80
+    print "LISTING OF ALL STUDENTS (BEST FIRST) WITH RAW GRADES:"
+    output = sys.stdout
+    print_output(output, names2, weights2, grades2, stu_order," ")
+    print "-"*80
+    output_filename = input_filename+".1.grades.rank.csv"
+    with open(output_filename,"w") as file:
+        print_output(file, names2, weights2, grades2, stu_order,", ")
+    print output_filename, "written."
+    print
+
+    print "-"*80
+    print "LISTING OF ALL STUDENTS (BEST FIRST) WITH SCALED SCORES:"
+    output = sys.stdout
+    print_output(output, names2, weights2, scores2, stu_order," ")
+    print "-"*80
+    output_filename = input_filename+".2.scores.rank.csv"
+    with open(output_filename,"w") as file:
+        print_output(file, names2, weights2, scores2, stu_order,", ")
+    print output_filename, "written."
+    print
+
+    # DROP WORST HOMEWORK, ETC...
+    scores3 = copy.deepcopy(scores)
+    scores3 = dropper.drop(names, weights, scores3)
+
+    # THEN RECOMPUTE WEIGHTED SCORES AND NEW RANKS
+    stu_order, wtd_score, ranks, scores3 = compute_ranks(names, weights, scores3)
+    names3, weights3, grades3, scores3 = augment(names, weights, grades, scores3, stu_order, wtd_score)
+
+    print "-"*80
+    print "LISTING OF ALL STUDENTS (BEST FIRST) WITH SCALED AND DROPPED SCORES:"
+    output = sys.stdout
+    print_output(output, names3, weights3, scores3, stu_order," ")
+    print "-"*80
+    output_filename = input_filename+".3.droppedscores.rank.csv"
+    with open(output_filename,"w") as file:
+        print_output(file, names3, weights3, scores3, stu_order,", ")
+    print output_filename, "written."
+    print
+
+        
+        
+        
+main()
+
+
